ea013b3a34fc59cbb23f5fc64234c7bdba27deec
Jens Kehne <kehne@kit.edu>
2017-03-21 11:20:24 +0100

    Infrastructure for hooking symbols loaded via dlsym()
    Plus hook function for cuMemAlloc_v2.
    Take that Nvidia :P

---
+++ dlsym_hook.cpp
@@ -0,0 +1,48 @@
+#include <dlfcn.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <unordered_map>
+
+typedef void *(*dlsym_fp)(void*, const char*);
+typedef void *(*dlopen_fp)(const char*, int);
+
+extern std::unordered_map<std::string, void *> fps;
+
+dlsym_fp real_dlsym = NULL;
+void *last_dlopen_handle = NULL;
+
+extern "C" {
+
+extern void *_dl_sym(void *, const char *, void *);
+void *dlsym(void *handle, const char *symbol) {
+  if (real_dlsym == NULL)
+    real_dlsym = reinterpret_cast<dlsym_fp>(_dl_sym(RTLD_NEXT, "dlsym", reinterpret_cast<void *>(dlsym)));
+
+  void *ret = NULL;
+  try {
+    ret = fps.at(symbol);
+  }
+  catch(std::out_of_range) {
+    ret = real_dlsym(handle, symbol);    
+  }
+
+  return ret;
+}
+
+void *dlopen(const char *filename, int flags) {
+  dlopen_fp real_dlopen;
+
+  if (real_dlsym == NULL)
+    real_dlsym = reinterpret_cast<dlsym_fp>(_dl_sym(RTLD_NEXT, "dlsym", reinterpret_cast<void *>(dlsym)));
+
+  real_dlopen = reinterpret_cast<dlopen_fp>(real_dlsym(RTLD_NEXT, "dlopen"));
+
+  last_dlopen_handle = real_dlopen(filename, flags);
+
+  return last_dlopen_handle;
+}
+
+}
--- malloc_hook.cpp
+++ malloc_hook.cpp
@@ -11,7 +11,12 @@ static size_t total_memory = 0;
 static std::unordered_map<void *, size_t> allocs;
 
 typedef int (*cuda_malloc_fp)(void**, size_t);
+typedef int (*cuda_mem_alloc_v2_fp)(unsigned int *, size_t);
 typedef int (*cuda_free_fp)(void*);
+typedef void *(*dlsym_fp)(void*, const char*);
+
+extern dlsym_fp real_dlsym;
+extern void *last_dlopen_handle;
 
 static void print_alloc_message(const std::string &str) {
   std::cerr << str << " Total memory allocated: " << total_memory << ", active buffers:" << active_buffers << std::endl;
@@ -23,7 +28,7 @@ int cudaMalloc(void **devPtr, size_t size) {
   int ret;
 
   cuda_malloc_fp orig_cuda_malloc;
-  orig_cuda_malloc = reinterpret_cast<cuda_malloc_fp>(dlsym(RTLD_NEXT, "cudaMalloc"));
+  orig_cuda_malloc = reinterpret_cast<cuda_malloc_fp>(real_dlsym(RTLD_NEXT, "cudaMalloc"));
 
   active_buffers++;
   total_memory += size;
@@ -39,7 +44,7 @@ int cudaMalloc(void **devPtr, size_t size) {
 
 int cudaFree(void *ptr) {
   cuda_free_fp orig_cuda_free;
-  orig_cuda_free = reinterpret_cast<cuda_free_fp>(dlsym(RTLD_NEXT, "cudaFree"));
+  orig_cuda_free = reinterpret_cast<cuda_free_fp>(real_dlsym(RTLD_NEXT, "cudaFree"));
 
   active_buffers--;
   total_memory -= allocs[ptr];
@@ -49,5 +54,27 @@ int cudaFree(void *ptr) {
 
   return orig_cuda_free(ptr);
 }
+
+int cuMemAlloc_v2(unsigned int *devPtr, size_t size) {
+  int ret;
+
+  cuda_mem_alloc_v2_fp orig_cuda_mem_alloc_v2;
+  orig_cuda_mem_alloc_v2 = reinterpret_cast<cuda_mem_alloc_v2_fp>(real_dlsym(last_dlopen_handle, "cuMemAlloc_v2"));
+
+  active_buffers++;
+  total_memory += size;
+
+  print_alloc_message("Allocation request.");
+
+  ret = orig_cuda_mem_alloc_v2(devPtr, size);
+
+  allocs[reinterpret_cast<void *>(*devPtr)] = size;
+
+  return ret;
+}
+
+std::unordered_map<std::string, void *> fps = {
+  {"cuMemAlloc_v2", reinterpret_cast<void *>(cuMemAlloc_v2)}
+};
 
 } /* extern "C" */
