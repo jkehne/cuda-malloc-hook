7f84b0a1ecae7810700b147c3dad6f5cca78ce85
Jens Kehne <kehne@kit.edu>
2017-03-21 12:13:11 +0100

    Implement hooks for the remaining allocation and free functions
    Also, ditch the libcudart functions, these call the ones in the driver API anyway.

--- malloc_hook.cpp
+++ malloc_hook.cpp
@@ -8,11 +8,12 @@
 static uint32_t active_buffers = 0;
 static size_t total_memory = 0;
 
-static std::unordered_map<void *, size_t> allocs;
+static std::unordered_map<uintptr_t, size_t> allocs;
 
-typedef int (*cuda_malloc_fp)(void**, size_t);
-typedef int (*cuda_mem_alloc_v2_fp)(unsigned int *, size_t);
-typedef int (*cuda_free_fp)(void*);
+typedef int (*cuda_mem_alloc_v2_fp)(uintptr_t*, size_t);
+typedef int (*cuda_mem_alloc_managed_fp)(uintptr_t*, size_t, unsigned int);
+typedef int (*cuda_mem_alloc_pitch_v2_fp)(uintptr_t*, size_t*, size_t, size_t, unsigned int);
+typedef int (*cuda_mem_free_v2_fp)(uintptr_t ptr);
 typedef void *(*dlsym_fp)(void*, const char*);
 
 extern dlsym_fp real_dlsym;
@@ -24,38 +25,7 @@ static void print_alloc_message(const std::string &str) {
 
 extern "C" {
 
-int cudaMalloc(void **devPtr, size_t size) {
-  int ret;
-
-  cuda_malloc_fp orig_cuda_malloc;
-  orig_cuda_malloc = reinterpret_cast<cuda_malloc_fp>(real_dlsym(RTLD_NEXT, "cudaMalloc"));
-
-  active_buffers++;
-  total_memory += size;
-
-  print_alloc_message("Allocation request.");
-
-  ret = orig_cuda_malloc(devPtr, size);
-
-  allocs[*devPtr] = size;
-
-  return ret;
-}
-
-int cudaFree(void *ptr) {
-  cuda_free_fp orig_cuda_free;
-  orig_cuda_free = reinterpret_cast<cuda_free_fp>(real_dlsym(RTLD_NEXT, "cudaFree"));
-
-  active_buffers--;
-  total_memory -= allocs[ptr];
-  allocs.erase(ptr);
-
-  print_alloc_message("Free request.");
-
-  return orig_cuda_free(ptr);
-}
-
-int cuMemAlloc_v2(unsigned int *devPtr, size_t size) {
+int cuMemAlloc_v2(uintptr_t *devPtr, size_t size) {
   int ret;
 
   cuda_mem_alloc_v2_fp orig_cuda_mem_alloc_v2;
@@ -68,13 +38,68 @@ int cuMemAlloc_v2(unsigned int *devPtr, size_t size) {
 
   ret = orig_cuda_mem_alloc_v2(devPtr, size);
 
-  allocs[reinterpret_cast<void *>(*devPtr)] = size;
+  allocs[*devPtr] = size;
 
   return ret;
 }
 
+int cuMemAllocManaged (uintptr_t *dptr, size_t size, unsigned int flags) {
+  int ret;
+
+  cuda_mem_alloc_managed_fp orig_cuda_mem_alloc_managed;
+  orig_cuda_mem_alloc_managed = reinterpret_cast<cuda_mem_alloc_managed_fp>(real_dlsym(last_dlopen_handle, "cuMemAllocManaged"));
+
+  active_buffers++;
+  total_memory += size;
+
+  print_alloc_message("Allocation request.");
+
+  ret = orig_cuda_mem_alloc_managed(dptr, size, flags);
+
+  allocs[*dptr] = size;
+
+  return ret;
+}
+
+int cuMemAllocPitch_v2 (uintptr_t* dptr, size_t* pPitch, size_t WidthInBytes, size_t Height, unsigned int  ElementSizeBytes) {
+  int ret;
+  size_t size;
+
+  cuda_mem_alloc_pitch_v2_fp orig_cuda_mem_alloc_pitch_v2;
+  orig_cuda_mem_alloc_pitch_v2 = reinterpret_cast<cuda_mem_alloc_pitch_v2_fp>(real_dlsym(last_dlopen_handle, "cuMemAllocPitch_v2"));
+
+  ret = orig_cuda_mem_alloc_pitch_v2(dptr, pPitch, WidthInBytes, Height, ElementSizeBytes);
+
+  size = *pPitch * Height;
+
+  active_buffers++;
+  total_memory += size;
+
+  print_alloc_message("Allocation request.");
+
+  allocs[*dptr] = size;
+
+  return ret;
+}
+
+int cuMemFree_v2(uintptr_t ptr) {
+  cuda_mem_free_v2_fp orig_cuda_mem_free_v2;
+  orig_cuda_mem_free_v2 = reinterpret_cast<cuda_mem_free_v2_fp>(real_dlsym(last_dlopen_handle, "cuMemFree_v2"));
+
+  active_buffers--;
+  total_memory -= allocs[ptr];
+  allocs.erase(ptr);
+
+  print_alloc_message("Free request.");
+
+  return orig_cuda_mem_free_v2(ptr);
+}
+
 std::unordered_map<std::string, void *> fps = {
-  {"cuMemAlloc_v2", reinterpret_cast<void *>(cuMemAlloc_v2)}
+  {"cuMemAlloc_v2", reinterpret_cast<void *>(cuMemAlloc_v2)},
+  {"cuMemAllocManaged", reinterpret_cast<void *>(cuMemAllocManaged)},
+  {"cuMemAllocPitch_v2", reinterpret_cast<void *>(cuMemAllocPitch_v2)},
+  {"cuMemFree_v2", reinterpret_cast<void *>(cuMemFree_v2)}
 };
 
 } /* extern "C" */
