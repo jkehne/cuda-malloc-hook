f8781d004828b1c00c54885c04a236977995e181
Jens Kehne <kehne@kit.edu>
2017-11-17 17:28:11 +0100

    Preserve rax and xmm0-7 as well
    Apparently, these can be used for parameters in some cases, and so far we've garbled them

--- generic_hook.cpp
+++ generic_hook.cpp
@@ -12,6 +12,8 @@ enum {
 
 static void generic_hook() {
   asm volatile(
+	       //preserve anything that may be a parameter
+               "push %%rax\n\t"
                "push %%rdi\n\t"
                "push %%rsi\n\t"
                "push %%rdx\n\t"
@@ -19,26 +21,49 @@ static void generic_hook() {
                "push %%r8\n\t"
                "push %%r9\n\t"
                "push %%r10\n\t"
+	       "subq $0x80, %%rsp\n\t"
+	       "movdqu %%xmm0, 0x0(%%rsp)\n\t"
+	       "movdqu %%xmm1, 0x10(%%rsp)\n\t"
+	       "movdqu %%xmm2, 0x20(%%rsp)\n\t"
+	       "movdqu %%xmm3, 0x30(%%rsp)\n\t"
+	       "movdqu %%xmm4, 0x40(%%rsp)\n\t"
+	       "movdqu %%xmm5, 0x50(%%rsp)\n\t"
+	       "movdqu %%xmm6, 0x60(%%rsp)\n\t"
+	       "movdqu %%xmm7, 0x70(%%rsp)\n\t"
+
+	       //do actual work
                "lea (%%rip), %%rdi\n\t"
                "and %0, %%rdi\n\t"
                "push %%rdi\n\t"
                "add %1, %%rdi\n\t"
-               "mov %%rdi, %%rax\n\t"
+               "mov %%rdi, %%r11\n\t"
                "add %1, %%rdi\n\t"
-               "callq *(%%rax)\n\t"
-               "pop %%rax\n\t"
-               "pop %%r10\n\t"
+               "callq *(%%r11)\n\t"
+	       "pop %%r11\n\t"
+
+	       //restore parameters and jump to hooked function
+	       "movdqu 0x0(%%rsp), %%xmm0\n\t"
+	       "movdqu 0x10(%%rsp), %%xmm1\n\t"
+	       "movdqu 0x20(%%rsp), %%xmm2\n\t"
+	       "movdqu 0x30(%%rsp), %%xmm3\n\t"
+	       "movdqu 0x40(%%rsp), %%xmm4\n\t"
+	       "movdqu 0x50(%%rsp), %%xmm5\n\t"
+	       "movdqu 0x60(%%rsp), %%xmm6\n\t"
+	       "movdqu 0x70(%%rsp), %%xmm7\n\t"
+	       "addq $0x80, %%rsp\n\t"
+	       "pop %%r10\n\t"
                "pop %%r9\n\t"
                "pop %%r8\n\t"
                "pop %%rcx\n\t"
                "pop %%rdx\n\t"
                "pop %%rsi\n\t"
                "pop %%rdi\n\t"
+               "pop %%rax\n\t"
                "pop %%rbp\n\t"
-               "jmpq *(%%rax)"
+               "jmpq *(%%r11)"
                :
                : "i" (~0xfff), "i" (sizeof(intptr_t))
-               : "rdi", "rax", "rsi", "rdx", "rcx", "r8", "r9", "r10"
+               : "rdi", "rax", "rsi", "rdx", "rcx", "r8", "r9", "r10", "r11", "xmm0", "xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7"
 	       );
 }
 
